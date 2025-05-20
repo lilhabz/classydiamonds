8e32635a6e0bd25f6d99746b7a31846a1d6321ec
lilhabz <mhabermehl8@gmail.com>
2025-05-20 19:12:02 +0000

    modified:   pages/account/edit.tsx

--- pages/account/edit.tsx
+++ pages/account/edit.tsx
@@ -86,8 +86,8 @@ export default function EditProfile() {
 
   return (
     <div className="bg-[#1f2a36] text-white min-h-screen px-4 py-10">
-      {/* 🧭 Breadcrumb container – full bleed + tight to navbar */}
-      <div className="pl-4 pr-4 sm:pl-8 sm:pr-8 mt-2 mb-4">
+      {/* 🧭 Breadcrumb container – pushed up under navbar */}
+      <div className="pl-4 pr-4 sm:pl-8 sm:pr-8 mb-6 -mt-4">
         <Breadcrumbs
           customLabels={{ account: "Account", edit: "Edit Profile" }}
         />
