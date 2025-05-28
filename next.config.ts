0fb7ed7b700faf581100ee90ec839751a8478301
lilhabz <mhabermehl8@gmail.com>
2025-05-28 17:50:59 +0000

    modified:   next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "res.cloudinary.com", // allow Cloudinary-hosted images
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
