a37d6f6e0fc2d018670ee3fa6463e18c4733093f
lilhabz <mhabermehl8@gmail.com>
2025-08-06 15:45:27 +0000

    modified:   pages/jewelry.tsx

--- pages/jewelry.tsx
+++ pages/jewelry.tsx
@@ -238,7 +238,7 @@ export default function JewelryPage({ products }: { products: ProductType[] }) {
           {filteredProducts.slice(0, visibleCount).map((product) => (
             <div
               key={product.id}
-              className="group bg-[var(--bg-nav)] rounded-2xl overflow-hidden shadow-md hover:shadow-2xl hover:scale-105 transition-transform duration-300 flex flex-col justify-between min-h-[388px]"
+              className="group bg-[var(--bg-nav)] w-[233.61px] h-[387.61px] rounded-2xl overflow-hidden shadow-md hover:shadow-2xl hover:scale-105 transition-transform duration-300 flex flex-col justify-between"
             >
               <Link
                 href={
